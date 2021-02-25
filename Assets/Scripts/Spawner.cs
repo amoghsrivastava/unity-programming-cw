355cfd709099c8e6a4ad75253281cf26e04ef88b
Amogh Srivastava <amoghsrivastava28@gmail.com>
2021-02-25 23:57:08 +0400

    Update Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,14 +8,14 @@ public class Spawner : MonoBehaviour
 
     public Enemy enemy;
     [Range(0, 25)]
-    public int numberOfEnemies = 10;
+    public int numberOfEnemies = 6;
     private float range;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        range = Random.Range(0.0f, 130.0f);
+        range = Random.Range(0.0f, 140.0f);
         for (int index = 0; index < numberOfEnemies; index++)
         {
             Enemy spawned = Instantiate(enemy, RandomNavmeshLocation(range), Quaternion.identity);
